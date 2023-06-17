3ee3894061e2ab728dd4096ee54eac7dd447844e
Shreya Patil <shreya170902@gmail.com>
2023-06-17 06:57:36 +0530

    Update EvenOdd_BruteForce.cpp

--- EvenOdd_BruteForce.cpp
+++ EvenOdd_BruteForce.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
 
+int EvenOdd(int n)
+{
+    if(n%2==0)
+    {
+        printf("The number is even");
+    }
+
+    else
+    {
+        printf("The number is odd");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    EvenOdd(n);
+}
